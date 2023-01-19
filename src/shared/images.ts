b666147f2aac89c62488e58bf273ed21c5c138c3
ken <82031953+ken-027@users.noreply.github.com>
2023-01-19 21:50:26 +0800

    images

---
+++ src/shared/images.ts
@@ -0,0 +1,189 @@
+import WorkOne1 from '../assets/work1/1.png'
+import WorkOne2 from '../assets/work1/2.png'
+import WorkOne3 from '../assets/work1/3.png'
+import WorkOne4 from '../assets/work1/4.png'
+import WorkOne5 from '../assets/work1/5.png'
+import WorkOne6 from '../assets/work1/6.png'
+import WorkOne7 from '../assets/work1/7.png'
+import WorkOne8 from '../assets/work1/8.png'
+import WorkOne9 from '../assets/work1/9.png'
+import WorkOne10 from '../assets/work1/10.png'
+import WorkOne11 from '../assets/work1/11.png'
+import WorkOne12 from '../assets/work1/12.png'
+import WorkOne13 from '../assets/work1/13.png'
+import WorkOne14 from '../assets/work1/14.png'
+import WorkOne15 from '../assets/work1/15.png'
+import WorkOne16 from '../assets/work1/16.png'
+import WorkOne17 from '../assets/work1/17.png'
+import WorkOne18 from '../assets/work1/18.png'
+import WorkOne19 from '../assets/work1/19.png'
+import WorkOne20 from '../assets/work1/20.png'
+import WorkOne21 from '../assets/work1/21.png'
+import WorkOne22 from '../assets/work1/22.png'
+import WorkOne23 from '../assets/work1/23.png'
+import WorkOne24 from '../assets/work1/24.png'
+import WorkOne25 from '../assets/work1/25.png'
+import WorkOne26 from '../assets/work1/26.png'
+import WorkOne27 from '../assets/work1/27.png'
+import WorkOne28 from '../assets/work1/28.png'
+import WorkOne29 from '../assets/work1/29.png'
+
+import WorkTwo1 from '../assets/work2/1.png'
+import WorkTwo2 from '../assets/work2/2.png'
+import WorkTwo3 from '../assets/work2/3.png'
+import WorkTwo4 from '../assets/work2/4.png'
+import WorkTwo5 from '../assets/work2/5.png'
+import WorkTwo6 from '../assets/work2/6.png'
+import WorkTwo7 from '../assets/work2/7.png'
+import WorkTwo8 from '../assets/work2/8.png'
+import WorkTwo9 from '../assets/work2/9.png'
+import WorkTwo10 from '../assets/work2/10.png'
+import WorkTwo11 from '../assets/work2/11.png'
+import WorkTwo12 from '../assets/work2/12.png'
+import WorkTwo13 from '../assets/work2/13.png'
+import error from '../assets/work2/error.png'
+
+import WorkThree1 from '../assets/work3/1.png'
+import WorkThree2 from '../assets/work3/2.png'
+import WorkThree3 from '../assets/work3/3.png'
+import WorkThree4 from '../assets/work3/4.png'
+import WorkThree5 from '../assets/work3/5.png'
+import WorkThree6 from '../assets/work3/6.png'
+import WorkThree7 from '../assets/work3/7.png'
+import WorkThree8 from '../assets/work3/8.png'
+import WorkThree9 from '../assets/work3/9.png'
+
+import WorkFour1 from '../assets/work4/1.png'
+import WorkFour2 from '../assets/work4/2.png'
+import WorkFour3 from '../assets/work4/3.png'
+import WorkFour4 from '../assets/work4/4.png'
+import WorkFour5 from '../assets/work4/5.png'
+import WorkFour6 from '../assets/work4/6.png'
+import WorkFour7 from '../assets/work4/7.png'
+import WorkFour8 from '../assets/work4/8.png'
+import WorkFour9 from '../assets/work4/9.png'
+import WorkFour10 from '../assets/work4/10.png'
+import WorkFour11 from '../assets/work4/11.png'
+
+import WorkFive1 from '../assets/work5/1.png'
+import WorkFive2 from '../assets/work5/2.png'
+import WorkFive3 from '../assets/work5/3.png'
+import WorkFive4 from '../assets/work5/4.png'
+import WorkFive5 from '../assets/work5/5.png'
+import WorkFive6 from '../assets/work5/6.png'
+import WorkFive7 from '../assets/work5/7.png'
+import WorkFive8 from '../assets/work5/8.png'
+import WorkFive9 from '../assets/work5/9.png'
+import WorkFive10 from '../assets/work5/10.png'
+import WorkFive11 from '../assets/work5/11.png'
+import WorkFive12 from '../assets/work5/12.png'
+import WorkFive13 from '../assets/work5/13.png'
+import WorkFive14 from '../assets/work5/14.png'
+import WorkFive15 from '../assets/work5/15.png'
+import WorkFive16 from '../assets/work5/16.png'
+import WorkFive17 from '../assets/work5/17.png'
+import WorkFive18 from '../assets/work5/18.png'
+import WorkFive19 from '../assets/work5/19.png'
+import WorkFive20 from '../assets/work5/20.png'
+import WorkFive21 from '../assets/work5/21.png'
+import WorkFive22 from '../assets/work5/22.png'
+
+export const workOne = [
+	WorkOne1,
+	WorkOne2,
+	WorkOne3,
+	WorkOne4,
+	WorkOne5,
+	WorkOne6,
+	WorkOne7,
+	WorkOne8,
+	WorkOne9,
+	WorkOne10,
+	WorkOne11,
+	WorkOne12,
+	WorkOne13,
+	WorkOne14,
+	WorkOne15,
+	WorkOne16,
+	WorkOne17,
+	WorkOne18,
+	WorkOne19,
+	WorkOne20,
+	WorkOne21,
+	WorkOne22,
+	WorkOne23,
+	WorkOne24,
+	WorkOne25,
+	WorkOne26,
+	WorkOne27,
+	WorkOne28,
+	WorkOne29,
+]
+
+export const workTwo = [
+	WorkTwo1,
+	WorkTwo2,
+	WorkTwo3,
+	WorkTwo4,
+	WorkTwo5,
+	WorkTwo6,
+	error,
+	WorkTwo7,
+	WorkTwo8,
+	WorkTwo9,
+	WorkTwo10,
+	WorkTwo11,
+	WorkTwo12,
+	WorkTwo13,
+]
+
+export const workThree = [
+	WorkThree1,
+	WorkThree2,
+	WorkThree3,
+	WorkThree4,
+	WorkThree5,
+	WorkThree6,
+	WorkThree7,
+	WorkThree8,
+	WorkThree9,
+]
+
+export const workFour = [
+	WorkFour1,
+	WorkFour2,
+	WorkFour3,
+	WorkFour4,
+	WorkFour5,
+	WorkFour6,
+	WorkFour7,
+	WorkFour8,
+	WorkFour9,
+	WorkFour10,
+	WorkFour11,
+]
+
+export const workFive = [
+	WorkFive1,
+	WorkFive2,
+	WorkFive3,
+	WorkFive4,
+	WorkFive5,
+	WorkFive6,
+	WorkFive7,
+	WorkFive8,
+	WorkFive9,
+	WorkFive10,
+	WorkFive11,
+	WorkFive12,
+	WorkFive13,
+	WorkFive14,
+	WorkFive15,
+	WorkFive16,
+	WorkFive17,
+	WorkFive18,
+	WorkFive19,
+	WorkFive20,
+	WorkFive21,
+	WorkFive22,
+]
